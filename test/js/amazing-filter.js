71c57e806efc5e436e919ec4c77673685ad68c9c
aaronfrost@ldschurch.org <nrmacsnrmnrmtm>
2013-03-14 09:14:52 -0600

    remove the test dir

--- test/js/amazing-filter.js
+++
@@ -1,7 +0,0 @@
-angular.module('app').filter('amazing', function () {
-  return function (input) {
-    if (input) {
-      return input.replace(/lame|boring|dull|pointless|slow/g, 'BLOODY AMAZING').replace(/sucked/g, 'was BLOODY AMAZING');
-    }
-  };
-});
