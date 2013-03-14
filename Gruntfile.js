8ad3d8e43d5bf0969471c8b6b3ece4ccb7a13a47
aaronfrost@ldschurch.org <nrmacsnrmnrmtm>
2013-03-13 20:14:54 -0700

    temp commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,35 @@
 module.exports = function(grunt){
 
 	grunt.initConfig({
-		//WE PUT OUR TASKS IN HERE
+		
+		regarde:{
+			dev:{
+				files:[
+					'js/**/*.js',
+					'css/**/*.css',
+					'templates/**/*.html'
+				],
+				tasks:['livereload']
+			}
+		}
+		//launch a tiny-lr server for livereload
+	    connect: {
+	      livereload: {
+	        options: {
+	          port: 9001,
+	          middleware: function(connect, options) {
+	            return [require('grunt-contrib-livereload/lib/utils').livereloadSnippet, (function(c,p) {
+	              return c.static(require('path').resolve(p));})(connect, '.')]
+	          }
+	        }
+	      }
+	    }
+
 	});
+
+	grunt.loadNpmTask('grunt-regarde');
+	grunt.loadNpmTask('grunt-contrib-connect');
+	grunt.loadNpmTask('grunt-contrib-livereload');
 
 	grunt.registerTask('build',[/* We can add our tasks from initConfig here */]);
 
