76b6d129864542c078dc45920cd82ff020df9f88
aaronfrost@ldschurch.org <nrmacsnrmnrmtm>
2013-03-13 20:08:42 -0700

    added some comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,9 @@
 module.exports = function(grunt){
 
 	grunt.initConfig({
-
+		//WE PUT OUR TASKS IN HERE
 	});
 
-	grunt.registerTask('build',[]);
+	grunt.registerTask('build',[/* We can add our tasks from initConfig here */]);
 
 }
